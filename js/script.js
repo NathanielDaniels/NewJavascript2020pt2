dbfae473657c4dbb18ed49e13bf73e47b1f29465
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-02-09 12:02:43 -0600

    adding styles to JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-const durationInput = document.querySelector('input')
-const startBtn = document.querySelector('.fa-play')
-const pauseBtn = document.querySelector('.fa-pause')
-startBtn.style.cursor = 'pointer'
+const container = document.querySelector('#timerContainer')
+const count = document.getElementById('count')
+const durationInput = document.getElementById('duration')
+const btns = document.getElementById('btns')
+const startBtn = document.getElementById('start')
+const pauseBtn = document.getElementById('pause')
+
+//! Styles
+document.body.style.height = '100vh'
+document.body.style.padding = '0'
+document.body.style.margin = '0'
+document.body.style.display = 'flex'
+document.body.style.alignItems = 'center'
+document.body.style.justifyContent = 'center'
+container.style.borderRadius = '50%'
+container.style.border = '2px solid black'
+container.style.height = '300px'
+container.style.width = '300px'
+container.style.display = 'flex'
+container.style.flexDirection = 'column'
+container.style.alignItems = 'center'
+container.style.justifyContent = 'center'
+startBtn.style.padding = '10px 15px'
+pauseBtn.style.padding = '10px 15px'
+durationInput.style.margin = '10px'
+durationInput.style.fontSize = '2.5rem'
+durationInput.style.width = '8rem'
 pauseBtn.style.cursor = 'pointer'
 
 class Timer {
@@ -10,21 +33,37 @@ class Timer {
     this.startBtn = startBtn
     this.pauseBtn = pauseBtn
 
+    // Call, Bind, Apply
+    // Call + Apply both run the function
+    // Bind only runs after the event is called (in the case, clicked)
+    //! Binding "this" connects "this" to start()
     this.durationInput.addEventListener('input', this.ondurationchange)
+    // this.startBtn.addEventListener('click', this.start.bind(this))
     this.startBtn.addEventListener('click', this.start)
     this.pauseBtn.addEventListener('click', this.pause)
   }
-  start () {
+  start = () => {
+    // this.importantMethodCall()
+    // THIS would not work without binding "this" to startBtn event listener
+    // Otherwise Arrow Function would do the same
     console.log('Play Button Clicked!')
   }
+  // importantMethodCall () {
+  //   console.log('Play Button Clicked!')
+  // }
   pause () {
     console.log('Pause Button Clicked!')
   }
   ondurationchange () {
     console.log('Input Clicked!')
-    console.log(inputType)
+    // console.log(durationInput.value)
+    for (let i = durationInput.value; i > 0; i--) {
+      // set a timer to reduce durationInput every 1000ms
+      // console.log(i)
+    }
+    count.append(durationInput.value)
   }
   tick () {}
 }
 
-const time = new Timer(durationInput, startBtn, pauseBtn)
+const timer = new Timer(durationInput, startBtn, pauseBtn)
