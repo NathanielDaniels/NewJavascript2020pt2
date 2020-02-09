a9e67c17dfa53ce9fe107b35a9427e51cd1d4e8b
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-02-09 07:19:50 -0600

    added buttons and input, linked to JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
+const durationInput = document.querySelector('input')
+const startBtn = document.querySelector('.fa-play')
+const pauseBtn = document.querySelector('.fa-pause')
+startBtn.style.cursor = 'pointer'
+pauseBtn.style.cursor = 'pointer'
 
+class Timer {
+  constructor (durationInput, startBtn, pauseBtn) {
+    this.durationInput = durationInput
+    this.startBtn = startBtn
+    this.pauseBtn = pauseBtn
 
-class Timer() {
-  constructor() {
-    // Constructor info
+    this.durationInput.addEventListener('input', this.ondurationchange)
+    this.startBtn.addEventListener('click', this.start)
+    this.pauseBtn.addEventListener('click', this.pause)
   }
-  start() {}
-  pause() {}
-  ondurationchange() {}
-  tick() {}
+  start () {
+    console.log('Play Button Clicked!')
+  }
+  pause () {
+    console.log('Pause Button Clicked!')
+  }
+  ondurationchange () {
+    console.log('Input Clicked!')
+    console.log(inputType)
+  }
+  tick () {}
 }
+
+const time = new Timer(durationInput, startBtn, pauseBtn)
