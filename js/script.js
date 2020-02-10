86aa491a088c8fb7dba3d1caa11620ae240002a7
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-02-10 16:19:34 -0600

    finished with timer app. just reformating

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 const container = document.querySelector('#timerContainer')
-// const count = document.getElementById('count')
 const durationInput = document.getElementById('duration')
 const btns = document.getElementById('btns')
 const startBtn = document.getElementById('start')
 const pauseBtn = document.getElementById('pause')
 const resetBtn = document.getElementById('reset')
 
-//! Styles
+//! Styles =========================
 document.body.style.height = '100vh'
 document.body.style.padding = '0'
 document.body.style.margin = '0'
@@ -30,64 +29,20 @@ durationInput.style.margin = '10px'
 durationInput.style.fontSize = '5rem'
 durationInput.style.textAlign = 'center'
 durationInput.style.width = '8rem'
-durationInput.style.height = '6rem'
+durationInput.style.height = '8rem'
+// durationInput.style.borderRadius = '40px'
 pauseBtn.style.cursor = 'pointer'
 startBtn.style.cursor = 'pointer'
+//! ====================================
 
-class Timer {
-  constructor (durationInput, startBtn, pauseBtn) {
-    this.durationInput = durationInput
-    this.startBtn = startBtn
-    this.pauseBtn = pauseBtn
-    this.resetBtn = resetBtn
-    //? Call, Bind, Apply
-    //? Call + Apply both run the function immediately
-    //? Bind only runs after the event is called (in this case, clicked)
-    //? Binding "this" connects "this" to start()
-    this.durationInput.addEventListener('input', this.ondurationchange)
-    this.startBtn.addEventListener('click', this.start)
-    this.pauseBtn.addEventListener('click', this.pause)
-    this.resetBtn.addEventListener('click', this.reset)
+const timer = new Timer(durationInput, startBtn, pauseBtn, {
+  onStart () {
+    console.log('timer started')
+  },
+  onTick () {
+    console.log('Ticking Down')
+  },
+  onComplete () {
+    alert('Fin')
   }
-  start = () => {
-    //? THIS would not work without binding "this" to startBtn event listener
-    //? Otherwise Arrow Function would do the same
-    // console.log('Play Button Clicked!')
-    this.tick()
-    this.interval = setInterval(this.tick, 1000)
-  }
-  pause = () => {
-    // console.log('Pause Button Clicked!')
-    clearInterval(this.interval)
-  }
-  ondurationchange = () => {
-    // console.log('Input Clicked!')
-    // console.log(durationInput.value)
-    // durationInput.value = this.timeLeft
-  }
-  tick = () => {
-    const timeRemaining = this.timeRemaining
-    if (this.timeRemaining <= 0) {
-      this.pause()
-      alert('Countdown Complete')
-    } else {
-      this.timeRemaining = timeRemaining - 1
-    }
-  }
-  //? Get and Set are used to store data in DOM
-  get timeRemaining () {
-    return parseFloat(this.durationInput.value)
-  }
-  set timeRemaining (time) {
-    this.durationInput.value = time
-  }
-  reset = () => {
-    //! Broken, need to reset Time after btn click
-    console.log('Reset Button Clicked!')
-    this.pause()
-    this.timeRemaining = durationInput.value
-    console.log(durationInput.value)
-  }
-}
-
-const timer = new Timer(durationInput, startBtn, pauseBtn)
+})
---
+++ js/timer.js
@@ -0,0 +1,73 @@
+class Timer {
+  constructor (durationInput, startBtn, pauseBtn, callbacks) {
+    this.durationInput = durationInput
+    this.startBtn = startBtn
+    this.pauseBtn = pauseBtn
+    this.resetBtn = resetBtn
+    if (callbacks) {
+      this.onStart = callbacks.onStart
+      this.onTick = callbacks.onTick
+      this.onComplete = callbacks.onComplete
+    }
+    //? Call, Bind, Apply
+    //? Call + Apply both run the function immediately
+    //? Bind only runs after the event is called (in this case, clicked)
+    //? Binding "this" connects "this" to start()
+    this.durationInput.addEventListener('input', this.ondurationchange)
+    this.startBtn.addEventListener('click', this.start)
+    this.pauseBtn.addEventListener('click', this.pause)
+    this.resetBtn.addEventListener('click', this.reset)
+  }
+  start = () => {
+    //? THIS would not work without binding "this" to startBtn event listener
+    //? Otherwise Arrow Function would do the same
+    // console.log('Play Button Clicked!')
+    if (this.onStart) {
+      this.onStart()
+      // console.log(this.onStart())
+    }
+
+    this.tick()
+    this.interval = setInterval(this.tick, 1000)
+  }
+  pause = () => {
+    // console.log('Pause Button Clicked!')
+    clearInterval(this.interval)
+  }
+  ondurationchange = () => {
+    // console.log('Input Clicked!')
+    // console.log(durationInput.value)
+    // durationInput.value = this.timeLeft
+  }
+  tick = () => {
+    const timeRemaining = this.timeRemaining
+    if (this.timeRemaining <= 0) {
+      this.pause()
+      if (this.onComplete) {
+        this.onComplete()
+      }
+      // alert('Countdown Complete')
+    } else {
+      this.timeRemaining = timeRemaining - 1
+      if (this.onTick) {
+        this.onTick()
+      }
+    }
+  }
+  //? Get and Set are used to store data in DOM
+  get timeRemaining () {
+    return parseFloat(this.durationInput.value)
+  }
+  set timeRemaining (time) {
+    this.durationInput.value = time
+    // console.log(this.timeRemaining)
+  }
+  reset = () => {
+    //! Broken, need to reset Time after btn click
+    console.log('Reset Button Clicked!')
+    this.pause()
+    this.timeRemaining = durationInput.value
+    console.log(durationInput.textinput)
+    console.log(this.timeRemaining)
+  }
+}
