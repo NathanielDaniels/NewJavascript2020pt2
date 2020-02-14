93acb1f9ec0daa36f75ee31411f48bb91003dac6
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-02-13 22:49:03 -0600

    setting up timeoutID

--- movieJS/script.js
+++ movieJS/script.js
@@ -2,14 +2,62 @@
 // const moviePosters = fetch("http://img.omdbapi.com/?apikey=[55cde2c2]&");
 // console.log(movieData);
 
-const fetchData = async () => {
+const fetchData = async searchTerm => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "55cde2c2",
-      s: "Avengers"
+      s: searchTerm
+      // i: "tt0848228"
+      // i: input
     }
   });
+
   console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector("input");
+let timeoutID;
+
+const onInput = e => {
+  if (timeoutID) {
+    clearTimeout(timeoutID);
+  }
+  timeoutID = setTimeout(() => {
+    // console.log(e.target.value);
+    fetchData(e.target.value);
+  }, 500);
+};
+
+input.addEventListener("input", onInput);
+
+//==================================================
+
+//! Set Movie Title
+// const title = response.data.Search[0].Title;
+// const grabTitle = document.querySelector("#movieTitle");
+// grabTitle.innerHTML = title;
+
+// //! Set Poster Image
+// const poster = response.data.Search[0].Poster;
+// const img = document.querySelector("img");
+// img.setAttribute("src", poster);
+// console.log(img);
+
+//==================================================
+// Practice
+
+// const fetchData2 = async () => {
+//   const response = await axios.get("http://www.omdbapi.com/", {
+//     params: {
+//       apikey: "55cde2c2",
+//       s: "Coraline"
+//     }
+//   });
+//   const poster = response.data.Search[1].Poster;
+//   const img = document.createElement("img");
+//   img.setAttribute("src", poster);
+//   console.log(img);
+//   document.body.append(img);
+// };
+
+// fetchData2();
