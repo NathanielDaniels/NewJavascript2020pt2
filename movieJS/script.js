31207580ccc004878f98c6cbfbad53391f158953
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-02-20 21:45:09 -0600

    swapping html for JS innerHTML

--- movieJS/script.js
+++ movieJS/script.js
@@ -2,6 +2,29 @@
 // const moviePosters = fetch("http://img.omdbapi.com/?apikey=[55cde2c2]&");
 // console.log(movieData);
 
+//!Dropdown Menu
+//? Select dropdown div
+//? create input
+//? handle input
+//? do search
+//? Add in html for menu
+//? Add in option to menu
+
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown is-active">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+
+`;
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
 const fetchData = async searchTerm => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
@@ -12,23 +35,34 @@ const fetchData = async searchTerm => {
 
   console.log(response.data.Error);
   if (response.data.Error) {
+    document.body.append(response.data.Error);
     return [];
   }
+
   return response.data.Search;
 };
 
-const input = document.querySelector("input");
+// if (input.value === "") {
+//   console.log("no input");
+// }
+
+// const input = document.querySelector("input");
 
 const onInput = async e => {
   const movies = await fetchData(e.target.value);
+
+  dropdown.classList.add("is-active");
+
   for (let movie of movies) {
     console.log(movie.Title);
-    const div = document.createElement("div");
-    div.innerHTML = `
-    <img src="${movie.Poster}" /
-    <h1>${movie.Title}</h1>
+    const option = document.createElement("a");
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+    <img src="${movie.Poster}" />
+    ${movie.Title}
+    ${option.classList.add("dropdown-divider")}
     `;
-    document.body.appendChild(div);
+    resultsWrapper.appendChild(option);
   }
 };
 
@@ -48,3 +82,20 @@ input.addEventListener("input", debounce(onInput, 500));
 // console.log(img);
 
 //==================================================
+// Practice
+
+// const fetchData2 = async () => {
+//   const response = await axios.get("http://www.omdbapi.com/", {
+//     params: {
+//       apikey: "55cde2c2",
+//       s: "Coraline"
+//     }
+//   });
+//   const poster = response.data.Search[1].Poster;
+//   const img = document.createElement("img");
+//   img.setAttribute("src", poster);
+//   console.log(img);
+//   document.body.append(img);
+// };
+
+// fetchData2();
