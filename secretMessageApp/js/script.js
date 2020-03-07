56b35b370106d189a079402269cd23ce6e7f59c4
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-03-07 12:51:08 -0600

    Toggling Element Visibility

--- secretMessageApp/js/script.js
+++ secretMessageApp/js/script.js
@@ -3,10 +3,11 @@ document.querySelector("form").addEventListener("submit", e => {
 
   const input = document.querySelector("#message-input");
   console.log(input.value);
-
   const encrypted = btoa(input.value);
 
-  document.querySelector("#link-input").value = encrypted;
+  const inputLink = document.querySelector("#link-input");
+  inputLink.value = `${window.location}#${encrypted}`;
+  inputLink.select();
 });
 
 // const change1 = btoa("Nathan");
