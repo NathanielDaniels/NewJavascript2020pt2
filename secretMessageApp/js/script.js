ba96048a42671533d678ab35e4c6a4c5b5b7b3ce
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-03-08 22:27:54 -0500

    changed background and link input

--- secretMessageApp/js/script.js
+++ secretMessageApp/js/script.js
@@ -1,8 +1,3 @@
-const card1 = document.querySelector(".card1");
-console.log(card1);
-const card2 = document.querySelector(".card2");
-console.log(card2);
-
 document.querySelector("form").addEventListener("submit", e => {
   e.preventDefault();
 
@@ -14,6 +9,8 @@ document.querySelector("form").addEventListener("submit", e => {
   inputLink.value = `${window.location}#${encrypted}`;
   inputLink.select();
 
+  const card1 = document.querySelector(".card1");
+  const card2 = document.querySelector(".card2");
   card1.classList.add("hidden");
   card2.classList.remove("hidden");
 });
