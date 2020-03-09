f90d18166496710313c6b069e336162536573bec
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-03-09 13:06:42 -0500

    upgraded decode popup w/ close button

--- secretMessageApp/js/script.js
+++ secretMessageApp/js/script.js
@@ -1,3 +1,7 @@
+const { hash } = window.location;
+
+console.log(atob(hash.replace("#", "")));
+
 const input = document.querySelector("#message-input");
 document.querySelector("form").addEventListener("submit", e => {
   e.preventDefault();
@@ -17,10 +21,20 @@ document.querySelector("form").addEventListener("submit", e => {
 const decodeBtn = document.querySelector("#decodeBtn");
 decodeBtn.addEventListener("click", () => {
   const newEl = document.createElement("div");
+  newEl.classList.add("card-panel", "z-depth-4");
   newEl.innerHTML = `${input.value}`;
   newEl.style =
-    "position: absolute; top: 300px; color: white; font-size: 2rem;";
-  // console.log(newEl);
+    "position: absolute;display: flex; justify-content: center; align-items: center; font-size: 2rem; background: white; color: black; padding: 100px 200px";
+
+  closeX = document.createElement("div");
+  closeX.innerHTML = "X";
+  closeX.style = "position: absolute; top: 0; right: 15px; cursor: pointer";
+  closeX.addEventListener("click", () => {
+    newEl.classList.add("hidden");
+  });
+  console.log(closeX);
+  newEl.append(closeX);
+
   document.body.appendChild(newEl);
 });
 
